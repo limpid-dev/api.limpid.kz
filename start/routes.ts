15347c7272b8898d0ba4f213541ecf687cb82293
Azamat <augustintherome@gmail.com>
2023-04-02 05:36:44 +0600

    Added user routes

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return 'Hello world from a slim app'
 })
+
+Route.post('/users', 'UsersController.store')
+Route.get('/users/:id', 'UsersController.show').middleware('auth')
