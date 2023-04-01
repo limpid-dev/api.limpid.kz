0284b45187a016021add357f30f9a24c3f9bdb87
Azamat <augustintherome@gmail.com>
2023-04-02 05:36:38 +0600

    Created user controller

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,7 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+}
