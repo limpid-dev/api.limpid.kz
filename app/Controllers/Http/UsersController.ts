f0a92889cf566cdeb7757608c6bc36df6a3a105f
Azamat <augustintherome@gmail.com>
2023-04-02 05:40:28 +0600

    Made show method for users

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import UsersShowValidator from 'App/Validators/UsersShowValidator'
 
 export default class UsersController {
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async show({ request }: HttpContextContract) {
+    const payload = await request.validate(UsersShowValidator)
+
+    return User.find(payload.id)
+  }
 }
