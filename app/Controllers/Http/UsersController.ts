49498537f6ac5357d24f436038d6108d6f16f306
Azamat <augustintherome@gmail.com>
2023-04-02 05:44:03 +0600

    Made users store method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import UsersShowValidator from 'App/Validators/UsersShowValidator'
+import UsersStoreValidator from 'App/Validators/UsersStoreValidator'
 
 export default class UsersController {
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const payload = await request.validate(UsersStoreValidator)
+
+    return User.create(payload)
+  }
 
   public async show({ request }: HttpContextContract) {
     const payload = await request.validate(UsersShowValidator)
