b91b5c37ce3d64f1187f4fa8ff6dca22c9f404a4
Azamat <augustintherome@gmail.com>
2023-04-02 15:17:05 +0600

    Modified fields to be not nullable in ban migration

--- database/migrations/1680408573453_bans.ts
+++ database/migrations/1680408573453_bans.ts
@@ -6,14 +6,20 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .notNullable()
       table.string('description', 255).notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
       table.timestamp('expired_at', { useTz: true }).nullable()
     })
   }
