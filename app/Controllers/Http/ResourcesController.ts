8bd2ee69de799f94b5f1f1145810bdf37f9de2b2
Azamat <augustintherome@gmail.com>
2023-04-02 23:44:09 +0600

    Added resources controller

---
+++ app/Controllers/Http/ResourcesController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ResourcesController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
