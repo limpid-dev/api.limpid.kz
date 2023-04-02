d9fb85e9f9e70d82f6d7f6a325ae008eb6cbe717
Azamat <augustintherome@gmail.com>
2023-04-02 07:24:15 +0600

    Updated user model to include relationship to token model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Token from './Token'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -20,6 +21,14 @@ export default class User extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Token)
+  public tokens: HasMany<typeof Token>
+
+  @hasMany(() => Token, {
+    onQuery: (query) => query.where('type', 'RECOVERY'),
+  })
+  public recoveryTokens: HasMany<typeof Token>
 
   @beforeSave()
   public static async hashPassword(user: User) {
