def21963b6acdf357d2ee3a32faf426e37435c30
Azamat <augustintherome@gmail.com>
2023-04-02 09:18:33 +0600

    Made update method on current user controller

--- app/Controllers/Http/UsersMeController.ts
+++ app/Controllers/Http/UsersMeController.ts
@@ -1,9 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UsersMeUpdateValidator from 'App/Validators/UsersMeUpdateValidator'
 
 export default class UsersMeController {
   public async index({ auth }: HttpContextContract) {
     return auth.user
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, auth }: HttpContextContract) {
+    const payload = await request.validate(UsersMeUpdateValidator)
+
+    await auth.user?.merge(payload).save()
+  }
 }
