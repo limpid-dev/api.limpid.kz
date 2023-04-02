03ea42dbabc9a0851e6f0ce0e36bafac8e3940e8
Azamat <augustintherome@gmail.com>
2023-04-03 00:03:20 +0600

    Made index method on resources controller

--- app/Controllers/Http/ResourcesController.ts
+++ app/Controllers/Http/ResourcesController.ts
@@ -1,7 +1,15 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ResourceIndexValidator from 'App/Validators/ResourceIndexValidator'
 
 export default class ResourcesController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(ResourceIndexValidator)
+
+    return await Database.from('resources')
+      .where('profile_id', payload.params.profileId)
+      .paginate(payload.page, payload.perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
