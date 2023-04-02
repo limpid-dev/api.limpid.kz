b418c78906312fd7b3f5f8115f7e720a76fc7b24
Azamat <augustintherome@gmail.com>
2023-04-02 10:20:29 +0600

    Added model for user bans

---
+++ app/Models/Ban.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Ban extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column.dateTime()
+  public expiredAt: DateTime | null
+
+  @column()
+  public description: string
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+}
