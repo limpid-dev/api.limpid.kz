eaa2cff1dd574a206e9b04ffcdfa5b7aa8e53e1f
Azamat <augustintherome@gmail.com>
2023-04-02 09:19:02 +0600

    Made confirmation middleware

--- app/Middleware/Confirmation.ts
+++ app/Middleware/Confirmation.ts
@@ -1,7 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ConfirmationValidator from 'App/Validators/ConfirmationValidator'
 
 export default class Confirmation {
-  public async handle({}: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ request, auth }: HttpContextContract, next: () => Promise<void>) {
+    const payload = await request.validate(ConfirmationValidator)
+
+    await auth.verifyCredentials(payload['@confirmation'].email, payload['@confirmation'].password)
+
     // code for middleware goes here. ABOVE THE NEXT CALL
     await next()
   }
