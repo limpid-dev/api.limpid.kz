76302e3d2f9de6679dfb247b4da10ba5c76ae439
Azamat <augustintherome@gmail.com>
2023-04-02 06:37:08 +0600

    Added login method to auth controller

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,18 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import AuthLoginValidator from 'App/Validators/AuthLoginValidator'
+
+export default class AuthController {
+  public async login({ request, auth }: HttpContextContract) {
+    const payload = await request.validate(AuthLoginValidator)
+
+    if (payload.mode === 'web') {
+      await auth.use('web').attempt(payload.email, payload.password)
+    }
+
+    if (payload.mode === 'api') {
+      const attempt = await auth.use('api').attempt(payload.email, payload.password)
+
+      return attempt.toJSON()
+    }
+  }
+}
