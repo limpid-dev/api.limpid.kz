7fc28326856b836db80cb0283e6672b617abe9cb
Azamat <augustintherome@gmail.com>
2023-04-02 13:43:31 +0600

    Removed crsf protection from login route

--- config/shield.ts
+++ config/shield.ts
@@ -91,7 +91,7 @@ export const csrf: ShieldConfig['csrf'] = {
 	| ```
   |
   */
-  exceptRoutes: [],
+  exceptRoutes: ['/auth/login'],
 
   /*
   |--------------------------------------------------------------------------
