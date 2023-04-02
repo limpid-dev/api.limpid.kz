ad1c435a667e5e645ea6c7efb99383adf753a3d3
Azamat <augustintherome@gmail.com>
2023-04-02 13:59:58 +0600

    Added guest middleware

--- app/Middleware/Guest.ts
+++ app/Middleware/Guest.ts
@@ -1,7 +1,17 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class GuestMiddleware {
-  public async handle({}: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
+    if (auth.isLoggedIn) {
+      response.forbidden({
+        errors: [
+          {
+            message: 'You are already logged in',
+          },
+        ],
+      })
+    }
+
     // code for middleware goes here. ABOVE THE NEXT CALL
     await next()
   }
