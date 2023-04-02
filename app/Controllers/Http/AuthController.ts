b00b4ec25f958fe54ef508ac50bcae94c6341df4
Azamat <augustintherome@gmail.com>
2023-04-02 12:54:52 +0600

    Modified login method of auth controller to bypass user model hooks and return appropriate error message

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,9 +1,43 @@
+import Hash from '@ioc:Adonis/Core/Hash'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import AuthLoginValidator from 'App/Validators/AuthLoginValidator'
+import { DateTime } from 'luxon'
 
 export default class AuthController {
-  public async login({ request, auth }: HttpContextContract) {
+  public async login({ request, auth, response }: HttpContextContract) {
     const payload = await request.validate(AuthLoginValidator)
+
+    const user = await Database.from('users').where('email', payload.email).firstOrFail()
+
+    const isValid = await Hash.verify(user.password, payload.password)
+
+    if (!isValid) {
+      response.badRequest({
+        errors: [
+          {
+            message: 'E_INVALID_AUTH_PASSWORD: Password mis-match',
+          },
+        ],
+      })
+    }
+
+    const isBanned = await Database.from('bans')
+      .from('bans')
+      .where('user_id', user.id)
+      .andWhere('expired_at', '>', DateTime.now().toSQL())
+      .orWhereNull('expired_at')
+      .first()
+
+    if (isBanned) {
+      response.forbidden({
+        errors: [
+          {
+            message: 'E_AUTHORIZATION_FAILURE: Banned access',
+          },
+        ],
+      })
+    }
 
     if (payload.mode === 'web') {
       await auth.use('web').attempt(payload.email, payload.password)
