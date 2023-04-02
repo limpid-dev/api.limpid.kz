6f594753f06a7256d43c1db7bb07b0bbf89f1b77
Azamat <augustintherome@gmail.com>
2023-04-02 08:32:07 +0600

    Created update method to recovery controller

--- app/Controllers/Http/RecoveryController.ts
+++ app/Controllers/Http/RecoveryController.ts
@@ -3,6 +3,8 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Token from 'App/Models/Token'
 import User from 'App/Models/User'
 import RecoveryStoreValidator from 'App/Validators/RecoveryStoreValidator'
+import RecoveryUpdateValidator from 'App/Validators/RecoveryUpdateValidator'
+import { DateTime } from 'luxon'
 
 export default class RecoveryController {
   public async store({ request }: HttpContextContract) {
@@ -15,5 +17,19 @@ export default class RecoveryController {
     await Mail.sendLater((message) => {
       message.from('info@limpid.kz').to(user.email).subject('Password recovery').text(token)
     })
+  }
+
+  public async update({ request }: HttpContextContract) {
+    const payload = await request.validate(RecoveryUpdateValidator)
+
+    const token = await Token.query()
+      .where('token', payload.params.token)
+      .andWhere('type', 'RECOVERY')
+      .andWhere('expiresAt', '>', DateTime.now().toSQL())
+      .preload('user')
+      .firstOrFail()
+
+    await token.user.merge({ password: payload.password }).save()
+    await token.delete()
   }
 }
