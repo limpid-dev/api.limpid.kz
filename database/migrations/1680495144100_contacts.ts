3223432c124ec0e7d2c318775182c97f459160d1
Azamat <augustintherome@gmail.com>
2023-04-03 11:43:12 +0600

    Updated contacts model migration

--- database/migrations/1680495144100_contacts.ts
+++ database/migrations/1680495144100_contacts.ts
@@ -6,12 +6,21 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table
+        .integer('profile_id')
+        .unsigned()
+        .references('id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+      table.string('type').notNullable()
+      table.string('name').notNullable()
+      table.string('value').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
