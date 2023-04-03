aeb4472803ec3771359f0edd307bf25d992dfe15
Azamat <augustintherome@gmail.com>
2023-04-03 11:45:49 +0600

    Made contacts index method validator

--- app/Validators/ContactsIndexValidator.ts
+++ app/Validators/ContactsIndexValidator.ts
@@ -1,4 +1,4 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { rules, schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ContactsIndexValidator {
@@ -23,7 +23,13 @@ export default class ContactsIndexValidator {
    *     ])
    *    ```
    */
-  public schema = schema.create({})
+  public schema = schema.create({
+    params: schema.object().members({
+      profileId: schema.number([rules.exists({ table: 'profiles', column: 'id' })]),
+    }),
+    page: schema.number([rules.unsigned()]),
+    perPage: schema.number([rules.unsigned()]),
+  })
 
   /**
    * Custom messages for validation failures. You can make use of dot notation `(.)`
