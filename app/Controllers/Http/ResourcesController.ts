b86bdeb6d255d61d488fffdfe1c4e82a05b535b6
Azamat <augustintherome@gmail.com>
2023-04-03 09:47:17 +0600

    Made update method on controller resource

--- app/Controllers/Http/ResourcesController.ts
+++ app/Controllers/Http/ResourcesController.ts
@@ -3,6 +3,7 @@ import Profile from 'App/Models/Profile'
 import Resource from 'App/Models/Resource'
 import ResourcesIndexValidator from 'App/Validators/ResourcesIndexValidator'
 import ResourcesStoreValidator from 'App/Validators/ResourcesStoreValidator'
+import ResourcesUpdateValidator from 'App/Validators/ResourcesUpdateValidator'
 
 export default class ResourcesController {
   public async index({ request }: HttpContextContract) {
@@ -33,7 +34,28 @@ export default class ResourcesController {
     })
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(ResourcesUpdateValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const resource = await Resource.findByOrFail('id', params.resourceId)
+
+        resource.merge(payload)
+
+        return await resource.save()
+      }
+    }
+    return response.forbidden({
+      errors: [
+        {
+          message: 'You are not allowed to update this resource',
+        },
+      ],
+    })
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
