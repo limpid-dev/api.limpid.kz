f1e4b7a09cfe2ed1727edbc4ff3aaa35778e6288
Azamat <augustintherome@gmail.com>
2023-04-03 11:47:10 +0600

    Made contacts controller index method

--- app/Controllers/Http/ContactsController.ts
+++ app/Controllers/Http/ContactsController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Contact from 'App/Models/Contact'
+import ContactsIndexValidator from 'App/Validators/ContactsIndexValidator'
 
 export default class ContactsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(ContactsIndexValidator)
+
+    return await Contact.query()
+      .where('profileId', payload.params.profileId)
+      .paginate(payload.page, payload.perPage)
+  }
 
   public async store({}: HttpContextContract) {}
 
