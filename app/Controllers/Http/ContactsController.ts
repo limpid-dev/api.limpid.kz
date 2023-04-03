67d098013695d6df5497b3285bd618de046ba6df
Azamat <augustintherome@gmail.com>
2023-04-03 12:16:55 +0600

    Made contacts controller store method

--- app/Controllers/Http/ContactsController.ts
+++ app/Controllers/Http/ContactsController.ts
@@ -1,6 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import Contact from 'App/Models/Contact'
+import Profile from 'App/Models/Profile'
 import ContactsIndexValidator from 'App/Validators/ContactsIndexValidator'
+import ContactsStoreValidator from 'App/Validators/ContactsStoreValidator'
+
+const ruleByType = {
+  EMAIL: rules.email(),
+  PHONE: rules.mobile({ strict: true }),
+  URL: rules.url(),
+} as const
 
 export default class ContactsController {
   public async index({ request }: HttpContextContract) {
@@ -11,7 +20,28 @@ export default class ContactsController {
       .paginate(payload.page, payload.perPage)
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(ContactsStoreValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        await request.validate({
+          schema: schema.create({
+            value: schema.string({}, [ruleByType[payload.type]]),
+          }),
+        })
+
+        return await Contact.create({
+          profileId: params.profileId,
+          ...payload,
+        })
+      }
+    }
+
+    return response.forbidden({})
+  }
 
   public async update({}: HttpContextContract) {}
 
