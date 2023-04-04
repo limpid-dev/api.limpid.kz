adb86a1bd232fceaa81128ae56955104ba8a15f6
Azamat <augustintherome@gmail.com>
2023-04-04 12:38:14 +0600

    Made migration for certificate model

--- database/migrations/1680589956540_certificates.ts
+++ database/migrations/1680589956540_certificates.ts
@@ -6,12 +6,26 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table
+        .integer('profile_id')
+        .unsigned()
+        .references('id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .notNullable()
+
+      table.string('title', 64).notNullable()
+      table.string('description', 256).notNullable()
+      table.string('institution', 64).notNullable()
+      table.json('attachment').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.date('issued_at').notNullable()
+      table.date('expired_at').nullable()
     })
   }
 
