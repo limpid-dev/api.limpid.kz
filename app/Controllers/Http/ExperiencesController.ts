d2c5ce7cdbcccaeaa0336dd935165a5e6d4bf44f
Azamat <augustintherome@gmail.com>
2023-04-04 10:51:56 +0600

    Made experience controller

--- app/Controllers/Http/ExperiencesController.ts
+++ app/Controllers/Http/ExperiencesController.ts
@@ -1,17 +1,66 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Experience from 'App/Models/Experience'
+import Profile from 'App/Models/Profile'
+import ExperiencesDestroyValidator from 'App/Validators/ExperiencesDestroyValidator'
+import ExperiencesIndexValidator from 'App/Validators/ExperiencesIndexValidator'
+import ExperiencesStoreValidator from 'App/Validators/ExperiencesStoreValidator'
+import ExperiencesUpdateValidator from 'App/Validators/ExperiencesUpdateValidator'
 
 export default class ExperiencesController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(ExperiencesIndexValidator)
 
-  public async create({}: HttpContextContract) {}
+    return await Experience.query()
+      .where('profileId', payload.params.profileId)
+      .paginate(payload.page, payload.perPage)
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(ExperiencesStoreValidator)
 
-  public async show({}: HttpContextContract) {}
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
 
-  public async edit({}: HttpContextContract) {}
+      if (profile.userId === auth.user.id) {
+        return await Experience.create({
+          profileId: params.profileId,
+          ...payload,
+        })
+      }
+    }
 
-  public async update({}: HttpContextContract) {}
+    return response.forbidden({})
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(ExperiencesUpdateValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const contact = await Experience.findOrFail(params.experienceId)
+
+        return await contact.merge(payload).save()
+      }
+    }
+
+    return response.forbidden({})
+  }
+
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { params } = await request.validate(ExperiencesDestroyValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const contact = await Experience.findOrFail(params.experienceId)
+
+        return await contact.delete()
+      }
+    }
+
+    return response.forbidden({})
+  }
 }
