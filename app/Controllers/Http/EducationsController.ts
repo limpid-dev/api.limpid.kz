6e4a68443aad45d6387eb1e8fe6f7f671632492b
Azamat <augustintherome@gmail.com>
2023-04-04 10:32:31 +0600

    Made education controller

--- app/Controllers/Http/EducationsController.ts
+++ app/Controllers/Http/EducationsController.ts
@@ -1,17 +1,66 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Education from 'App/Models/Education'
+import Profile from 'App/Models/Profile'
+import EducationsDestroyValidator from 'App/Validators/EducationsDestroyValidator'
+import EducationsIndexValidator from 'App/Validators/EducationsIndexValidator'
+import EducationsStoreValidator from 'App/Validators/EducationsStoreValidator'
+import EducationsUpdateValidator from 'App/Validators/EducationsUpdateValidator'
 
 export default class EducationsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(EducationsIndexValidator)
 
-  public async create({}: HttpContextContract) {}
+    return await Education.query()
+      .where('profileId', payload.params.profileId)
+      .paginate(payload.page, payload.perPage)
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(EducationsStoreValidator)
 
-  public async show({}: HttpContextContract) {}
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
 
-  public async edit({}: HttpContextContract) {}
+      if (profile.userId === auth.user.id) {
+        return await Education.create({
+          profileId: params.profileId,
+          ...payload,
+        })
+      }
+    }
 
-  public async update({}: HttpContextContract) {}
+    return response.forbidden({})
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(EducationsUpdateValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const contact = await Education.findOrFail(params.educationId)
+
+        return await contact.merge(payload).save()
+      }
+    }
+
+    return response.forbidden({})
+  }
+
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { params } = await request.validate(EducationsDestroyValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const contact = await Education.findOrFail(params.educationId)
+
+        return await contact.delete()
+      }
+    }
+
+    return response.forbidden({})
+  }
 }
