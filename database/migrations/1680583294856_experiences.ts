f13f554ff4de03422f6683de3bc89e8f5586d0a3
Azamat <augustintherome@gmail.com>
2023-04-04 10:46:41 +0600

    Made migration for experience model

--- database/migrations/1680583294856_experiences.ts
+++ database/migrations/1680583294856_experiences.ts
@@ -3,19 +3,31 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'experiences'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table
+        .integer('profile_id')
+        .unsigned()
+        .references('id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+
+      table.string('title', 64).notNullable()
+      table.string('description', 256).notNullable()
+      table.string('organization', 64).notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.date('started_at').notNullable()
+      table.date('finished_at').nullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
