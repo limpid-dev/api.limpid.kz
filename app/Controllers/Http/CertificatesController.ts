a21180354ec7d8d1084e65fe9f2f956f11288a6c
Azamat <augustintherome@gmail.com>
2023-04-04 12:47:26 +0600

    Made certificates controller

--- app/Controllers/Http/CertificatesController.ts
+++ app/Controllers/Http/CertificatesController.ts
@@ -1,17 +1,73 @@
+import { Attachment } from '@ioc:Adonis/Addons/AttachmentLite'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Certificate from 'App/Models/Certificate'
+import Profile from 'App/Models/Profile'
+import CertificatesDestroyValidator from 'App/Validators/CertificatesDestroyValidator'
+import CertificatesIndexValidator from 'App/Validators/CertificatesIndexValidator'
+import CertificatesStoreValidator from 'App/Validators/CertificatesStoreValidator'
+import CertificatesUpdateValidator from 'App/Validators/CertificatesUpdateValidator'
 
 export default class CertificatesController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(CertificatesIndexValidator)
 
-  public async create({}: HttpContextContract) {}
+    return await Certificate.query()
+      .where('profileId', payload.params.profileId)
+      .paginate(payload.page, payload.perPage)
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, auth, response }: HttpContextContract) {
+    const { params, attachment, ...payload } = await request.validate(CertificatesStoreValidator)
 
-  public async show({}: HttpContextContract) {}
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
 
-  public async edit({}: HttpContextContract) {}
+      if (profile.userId === auth.user.id) {
+        return await Certificate.create({
+          profileId: params.profileId,
+          attachment: Attachment.fromFile(attachment),
+          ...payload,
+        })
+      }
+    }
 
-  public async update({}: HttpContextContract) {}
+    return response.forbidden({})
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ request, auth, response }: HttpContextContract) {
+    const { params, attachment, ...payload } = await request.validate(CertificatesUpdateValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const certificate = await Certificate.findOrFail(params.certificateId)
+
+        return await certificate
+          .merge({
+            attachment: attachment ? Attachment.fromFile(attachment) : certificate.attachment,
+            ...payload,
+          })
+          .save()
+      }
+    }
+
+    return response.forbidden({})
+  }
+
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { params } = await request.validate(CertificatesDestroyValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const certificate = await Certificate.findOrFail(params.certificateId)
+
+        return await certificate.delete()
+      }
+    }
+
+    return response.forbidden({})
+  }
 }
