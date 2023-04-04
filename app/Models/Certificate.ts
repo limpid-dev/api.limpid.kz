3d054a921de5d0280367bf2ff7ec7b0ab1160c84
Azamat <augustintherome@gmail.com>
2023-04-04 12:35:39 +0600

    Made certificate model

--- app/Models/Certificate.ts
+++ app/Models/Certificate.ts
@@ -1,13 +1,35 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Profile from './Profile'
 
 export default class Certificate extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+  @column()
+  public institution: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @column.dateTime()
+  public issuedAt: DateTime
+
+  @column.dateTime()
+  public expiredAt: DateTime | null
+
+  @column()
+  public profileId: number
+
+  @belongsTo(() => Profile)
+  public profile: BelongsTo<typeof Profile>
 }
