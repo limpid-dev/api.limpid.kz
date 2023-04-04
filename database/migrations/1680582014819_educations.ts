08d987ef32ca06b2b2d2cd53f77193de1907951a
Azamat <augustintherome@gmail.com>
2023-04-04 10:24:10 +0600

    Made migration for education model

--- database/migrations/1680582014819_educations.ts
+++ database/migrations/1680582014819_educations.ts
@@ -6,12 +6,24 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table
+        .integer('profile_id')
+        .unsigned()
+        .references('id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+
+      table.string('title').notNullable()
+      table.string('description').notNullable()
+      table.string('institution').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('started_at', { useTz: true }).notNullable()
+      table.timestamp('finished_at', { useTz: true }).nullable()
     })
   }
 
