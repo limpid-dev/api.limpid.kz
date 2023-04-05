d64c4a87526e512ed0fe2979c4bbb4dc8fdce6b8
Azamat <augustintherome@gmail.com>
2023-04-05 12:19:02 +0600

    Made skills controller

--- app/Controllers/Http/SkillsController.ts
+++ app/Controllers/Http/SkillsController.ts
@@ -1,11 +1,83 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Profile from 'App/Models/Profile'
+import Skill from 'App/Models/Skill'
+import SkillsDestroyValidator from 'App/Validators/SkillsDestroyValidator'
+import SkillsIndexValidator from 'App/Validators/SkillsIndexValidator'
+import SkillsStoreValidator from 'App/Validators/SkillsStoreValidator'
+import SkillsUpdateValidator from 'App/Validators/SkillsUpdateValidator'
 
 export default class SkillsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(SkillsIndexValidator)
+    const profile = await Profile.findOrFail(params.profileId)
 
-  public async store({}: HttpContextContract) {}
+    return await profile.related('skills').query().paginate(payload.page, payload.perPage)
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async store({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(SkillsStoreValidator)
 
-  public async destroy({}: HttpContextContract) {}
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        return Skill.create({ profileId: params.profileId, ...payload })
+      }
+    }
+
+    return response.forbidden({
+      errors: [
+        {
+          message: 'You are not allowed to create a skill for this profile',
+        },
+      ],
+    })
+  }
+
+  public async update({ request, auth, response }: HttpContextContract) {
+    const { params, ...payload } = await request.validate(SkillsUpdateValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const resource = await Skill.findByOrFail('id', params.skillId)
+
+        resource.merge(payload)
+
+        return await resource.save()
+      }
+    }
+    return response.forbidden({
+      errors: [
+        {
+          message: 'You are not allowed to update this skil',
+        },
+      ],
+    })
+  }
+
+  public async destroy({ request, auth, response }: HttpContextContract) {
+    const { params } = await request.validate(SkillsDestroyValidator)
+
+    if (auth.user) {
+      const profile = await Profile.findOrFail(params.profileId)
+
+      if (profile.userId === auth.user.id) {
+        const resource = await Skill.findByOrFail('id', params.skillId)
+
+        await resource.delete()
+
+        return response.gone()
+      }
+    }
+
+    return response.forbidden({
+      errors: [
+        {
+          message: 'You are not allowed to delete this skill',
+        },
+      ],
+    })
+  }
 }
