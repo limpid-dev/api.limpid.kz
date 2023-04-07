b300462f4e389a98916eb4f75b20fc70af7651a1
Azamat <augustintherome@gmail.com>
2023-04-07 16:49:27 +0600

    Updated file model migration

---
+++ database/migrations/1680864103861_files.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'files'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.integer('size').notNullable()
+      table.string('extname').notNullable()
+      table.string('mime_type').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
