4ae72fee3ad0929e7850161819d545f63d0dd96f
Azamat <augustintherome@gmail.com>
2023-04-08 22:44:08 +0600

    Cleaned up routes

--- start/routes.ts
+++ start/routes.ts
@@ -20,61 +20,39 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/users/@me', 'UsersMeController.index').middleware('auth')
-Route.patch('/users/@me', 'UsersMeController.update').middleware('auth')
+Route.resource('users', 'UsersController').only(['store', 'show', 'update']).middleware({
+  store: 'guest',
+  show: 'auth',
+  update: 'auth',
+})
 
-Route.post('/users', 'UsersController.store').middleware('guest')
-Route.get('/users/:userId', 'UsersController.show').middleware('auth')
+Route.resource('sessions', 'SessionsController').only(['store', 'show', 'destroy']).middleware({
+  store: 'guest',
+  show: 'auth',
+  destroy: 'auth',
+})
 
-Route.post('/sessions', 'SessionsController.store').middleware('guest')
-Route.delete('/sessions', 'SessionsController.destroy').middleware('auth')
+Route.resource('recoveries', 'RecoveriesController').only(['store', 'show']).middleware({
+  '*': 'guest',
+})
 
-Route.post('/recoveries', 'RecoveriesController.store').middleware('guest')
-Route.post('/recoveries/:token', 'RecoveriesController.update').middleware('guest')
-
-Route.post('/verifications', 'VerificationsController.store').middleware('auth')
-Route.patch('/verifications/:token', 'VerificationsController.update').middleware('auth')
-
-Route.resource('profiles', 'ProfilesController')
-  .apiOnly()
-  .paramFor('profiles', 'profileId')
-  .middleware({
-    '*': ['auth', 'verification'],
-  })
+Route.resource('verifications', 'VerificationsController').only(['store', 'show']).middleware({
+  '*': 'auth',
+})
 
 Route.group(() => {
-  Route.resource('resources', 'ResourcesController').apiOnly().paramFor('resources', 'resourceId')
+  Route.resource('profiles', 'ProfilesController').apiOnly()
 
-  Route.resource('contacts', 'ContactsController').apiOnly().paramFor('contacts', 'contactId')
+  Route.resource('profiles.resources', 'ResourcesController').apiOnly()
+  Route.resource('profiles.contacts', 'ContactsController').apiOnly()
+  Route.resource('profiles.educations', 'EducationsController').apiOnly()
+  Route.resource('profiles.experiences', 'ExperiencesController').apiOnly()
+  Route.resource('profiles.certificates', 'CertificatesController').apiOnly()
+  Route.resource('profiles.skills', 'SkillsController').apiOnly()
 
-  Route.resource('educations', 'EducationsController')
-    .apiOnly()
-    .paramFor('educations', 'educationId')
+  Route.resource('projects', 'ProjectsController').apiOnly()
 
-  Route.resource('experiences', 'ExperiencesController')
-    .apiOnly()
-    .paramFor('experiences', 'experienceId')
-
-  Route.resource('certificates', 'CertificatesController')
-    .apiOnly()
-    .paramFor('certificates', 'certificateId')
-
-  Route.resource('skills', 'SkillsController').apiOnly().paramFor('skills', 'skillId')
+  Route.resource('projects.memberships', 'MembershipsController').apiOnly()
 })
-  .prefix('/profiles/:profileId')
-  .middleware(['auth', 'verification'])
-
-Route.resource('projects', 'ProjectsController')
-  .apiOnly()
-  .paramFor('projects', 'projectId')
-  .middleware({
-    '*': ['auth', 'verification'],
-  })
-
-Route.group(() => {
-  Route.resource('memberships', 'MembershipsController')
-    .apiOnly()
-    .paramFor('memberships', 'membershipId')
-})
-  .prefix('/projects/:projectId')
-  .middleware(['auth', 'verification'])
+  .middleware('auth')
+  .middleware('verification')
