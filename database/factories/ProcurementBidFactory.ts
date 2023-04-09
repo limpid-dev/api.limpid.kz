5dca7c409384ffa43e4d297e576331a180d3a121
Azamat <augustintherome@gmail.com>
2023-04-09 19:44:20 +0600

    Created procurement bid resource

---
+++ database/factories/ProcurementBidFactory.ts
@@ -0,0 +1,8 @@
+import ProcurementBid from 'App/Models/ProcurementBid'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(ProcurementBid, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
