a9d254548258ef358972e2c20cc07407d14b3193
Azamat <augustintherome@gmail.com>
2023-04-09 18:12:57 +0600

    Added procurement relation to file model

--- app/Models/File.ts
+++ app/Models/File.ts
@@ -2,6 +2,7 @@ import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 import Certificate from './Certificate'
 import Project from './Project'
+import Procurement from './Procurement'
 
 export default class File extends BaseModel {
   @column({ isPrimary: true })
@@ -42,4 +43,10 @@ export default class File extends BaseModel {
 
   @belongsTo(() => Project)
   public project: BelongsTo<typeof Project>
+
+  @column()
+  public procurementId: number | null
+
+  @belongsTo(() => Procurement)
+  public procurement: BelongsTo<typeof Procurement>
 }
