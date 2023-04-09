eb92594da7d8f065c2ef4c35b59017ef6187b20c
Azamat <augustintherome@gmail.com>
2023-04-09 10:01:30 +0600

    Created user factory

---
+++ database/factories/UserFactory.ts
@@ -0,0 +1,18 @@
+import User from 'App/Models/User'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import { DateTime } from 'luxon'
+
+export default Factory.define(User, ({ faker }) => {
+  return {
+    bornAt: DateTime.now(),
+    id: Number(faker.random.numeric(10)),
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    rememberMeToken: null,
+    verifiedAt: DateTime.now(),
+  }
+}).build()
