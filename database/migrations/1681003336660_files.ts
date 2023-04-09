088dc883c0a0b14a6875c4a4b7604906e6de01cb
Azamat <augustintherome@gmail.com>
2023-04-09 07:29:35 +0600

    Updated file migration

--- database/migrations/1681003336660_files.ts
+++ database/migrations/1681003336660_files.ts
@@ -3,19 +3,23 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'files'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name', 64).notNullable()
+      table.integer('size').notNullable()
+      table.string('mimeType', 64).notNullable()
+      table.string('extname', 64).notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
