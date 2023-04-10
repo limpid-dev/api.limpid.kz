cc2de338782baadc316ba76dc5933935117f3d07
Azamat <augustintherome@gmail.com>
2023-04-10 19:34:20 +0600

    Added profile relation to auction model

--- app/Models/Auction.ts
+++ app/Models/Auction.ts
@@ -1,10 +1,17 @@
 import { DateTime } from 'luxon'
-import { BaseModel, HasMany, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, HasMany, belongsTo, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import File from './File'
+import Profile from './Profile'
 
 export default class Auction extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public profileId: number
+
+  @belongsTo(() => Profile)
+  public profile: BelongsTo<typeof Profile>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
