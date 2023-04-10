21b271b1ba51434b2ac9d5bfe15876fd235229b6
Azamat <augustintherome@gmail.com>
2023-04-10 19:40:12 +0600

    Made auctions controller

--- app/Controllers/Http/AuctionsController.ts
+++ app/Controllers/Http/AuctionsController.ts
@@ -1,3 +1,63 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { bind } from '@adonisjs/route-model-binding'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Auction from 'App/Models/Auction'
+import Profile from 'App/Models/Profile'
+import AuctionStoreValidator from 'App/Validators/AuctionStoreValidator'
+import AuctionUpdateValidator from 'App/Validators/AuctionUpdateValidator'
+import PaginationValidator from 'App/Validators/PaginationValidator'
+import ProfileActionValidator from 'App/Validators/ProfileActionValidator'
 
-export default class AuctionsController {}
+export default class AuctionsController {
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(PaginationValidator)
+
+    return await Auction.query().paginate(payload.page, payload.perPage)
+  }
+
+  @bind()
+  public async show({}: HttpContextContract, auction: Auction) {
+    return {
+      data: auction,
+    }
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const payload = await request.validate(AuctionStoreValidator)
+    const { profileId } = await request.validate(ProfileActionValidator)
+
+    const profile = await Profile.findOrFail(profileId)
+
+    const auction = await profile.related('auctions').create(payload)
+
+    return {
+      data: auction,
+    }
+  }
+
+  @bind()
+  public async update({ request, bouncer }: HttpContextContract, auction: Auction) {
+    const payload = await request.validate(AuctionUpdateValidator)
+    const { profileId } = await request.validate(ProfileActionValidator)
+
+    const profile = await Profile.findOrFail(profileId)
+
+    await bouncer.with('AuctionPolicy').authorize('update', profile, auction)
+
+    await auction.merge(payload).save()
+
+    return {
+      data: auction,
+    }
+  }
+
+  @bind()
+  public async destroy({ request, bouncer }: HttpContextContract, auction: Auction) {
+    const { profileId } = await request.validate(ProfileActionValidator)
+
+    const profile = await Profile.findOrFail(profileId)
+
+    await bouncer.with('AuctionPolicy').authorize('delete', profile, auction)
+
+    await auction.delete()
+  }
+}
