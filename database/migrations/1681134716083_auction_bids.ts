17401002cbd3f32d1d23868d0bae5bb46fd0c315
Azamat <augustintherome@gmail.com>
2023-04-10 20:14:54 +0600

    Updated auction bid migration

--- database/migrations/1681134716083_auction_bids.ts
+++ database/migrations/1681134716083_auction_bids.ts
@@ -3,19 +3,35 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'auction_bids'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table
+        .integer('auction_id')
+        .unsigned()
+        .references('id')
+        .inTable('auctions')
+        .onDelete('CASCADE')
+        .notNullable()
+      table
+        .integer('profile_id')
+        .unsigned()
+        .references('id')
+        .inTable('profiles')
+        .onDelete('CASCADE')
+        .notNullable()
+
+      table.integer('price').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
