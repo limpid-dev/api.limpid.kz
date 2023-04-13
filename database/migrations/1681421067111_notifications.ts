8837413ccb96fe8f9cce2b9c16c6868f7ac32620
Azamat <augustintherome@gmail.com>
2023-04-14 03:40:34 +0600

    Updated notification model migration

--- database/migrations/1681421067111_notifications.ts
+++ database/migrations/1681421067111_notifications.ts
@@ -3,19 +3,24 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'notifications'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable()
+      table.string('title', 64).notNullable()
+      table.string('body', 256).notNullable()
+      table.json('data').nullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('read_at', { useTz: true }).nullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
