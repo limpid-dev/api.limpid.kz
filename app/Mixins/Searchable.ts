4e3a7020ce2ed3e61029dbb0fc368ef29610f345
Azamat <augustintherome@gmail.com>
2023-04-13 20:43:10 +0600

    Fixed searchable mixin

--- app/Mixins/Searchable.ts
+++ app/Mixins/Searchable.ts
@@ -4,16 +4,5 @@ import { NormalizeConstructor } from '@ioc:Adonis/Core/Helpers'
 export const Searchable = <T extends NormalizeConstructor<typeof BaseModel>>(superclass: T) => {
   return class extends superclass {
     public static search: string[] = []
-    private searchSet: Set<string>
-
-    public get search() {
-      if (this.searchSet) {
-        return this.searchSet
-      }
-
-      this.searchSet = new Set(this.search)
-
-      return this.searchSet
-    }
   }
 }
