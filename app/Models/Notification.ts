b083e54095c7b47b491dcf41184b3cb80be1eeac
Azamat <augustintherome@gmail.com>
2023-04-14 03:36:42 +0600

    Made notification model

--- app/Models/Notification.ts
+++ app/Models/Notification.ts
@@ -1,5 +1,11 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export type JSONPrimitive = string | number | boolean | null
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+export type JSONObject = { [member: string]: JSONValue }
+export interface JSONArray extends Array<JSONValue> {}
 
 export default class Notification extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +16,25 @@ export default class Notification extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @column.dateTime()
+  public readAt: DateTime | null
+
+  @column()
+  public title: string
+
+  @column()
+  public body: string
+
+  @column({
+    prepare: (value: unknown) => JSON.stringify(value),
+    consume: (value: unknown) => (typeof value === 'string' ? JSON.parse(value) : value),
+  })
+  public data: Record<string, unknown>
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
