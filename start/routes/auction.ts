c777126fd32ae20ab47f34114078b2627c04dd21
Azamat <augustintherome@gmail.com>
2023-04-13 09:26:41 +0600

    Fixed scoped params in auction routes

--- start/routes/auction.ts
+++ start/routes/auction.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('auctions', 'AuctionsController.index')
-Route.get('auctions/:>auction', 'AuctionsController.show')
+Route.get('auctions/:auction', 'AuctionsController.show')
 Route.post('auctions', 'AuctionsController.store').middleware('auth')
-Route.delete('auctions/:>auction', 'AuctionsController.destroy').middleware('auth')
+Route.delete('auctions/:auction', 'AuctionsController.destroy').middleware('auth')
