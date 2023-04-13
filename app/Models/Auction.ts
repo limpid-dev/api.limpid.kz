c8bca6b1d49774a2173a577dcb88cde817598b0d
Azamat <augustintherome@gmail.com>
2023-04-13 21:34:12 +0600

    Added qs to auctions

--- app/Models/Auction.ts
+++ app/Models/Auction.ts
@@ -1,10 +1,14 @@
+import { compose } from '@ioc:Adonis/Core/Helpers'
 import { DateTime } from 'luxon'
 import { BaseModel, BelongsTo, HasMany, belongsTo, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import File from './File'
 import Profile from './Profile'
 import AuctionBid from './AuctionBid'
+import { Searchable } from 'App/Mixins/Searchable'
 
-export default class Auction extends BaseModel {
+export default class Auction extends compose(BaseModel, Searchable) {
+  public static search = ['title', 'description']
+
   @column({ isPrimary: true })
   public id: number
 
