8ddf24485572ce62f7da59e4709c46b57a9a6bd3
Azamat <augustintherome@gmail.com>
2023-04-23 22:17:03 +0600

    Created tender bid model

---
+++ app/Models/TenderBid.ts
@@ -0,0 +1,33 @@
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import Profile from './Profile'
+import Tender from './Tender'
+
+export default class TenderBid extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column.dateTime()
+  public wondAt: DateTime | null
+
+  @column()
+  public tenderId: number
+
+  @belongsTo(() => Tender)
+  public tender: BelongsTo<typeof Tender>
+
+  @column()
+  public profileId: number
+
+  @belongsTo(() => Profile)
+  public profile: BelongsTo<typeof Profile>
+
+  @column()
+  public price: number
+}
