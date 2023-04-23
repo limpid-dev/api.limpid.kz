63fafc563bd18e0b1fb57a9cdd9e4fc474353536
Azamat <augustintherome@gmail.com>
2023-04-23 22:48:38 +0600

    Removed tenders update method from routes

--- start/routes/tender.ts
+++ start/routes/tender.ts
@@ -3,5 +3,4 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/tenders', 'TendersController.index')
 Route.get('/tenders/:tender', 'TendersController.show')
 Route.post('/tenders', 'TendersController.store').middleware('auth:web,api')
-Route.patch('/tenders/:tender', 'TendersController.update').middleware('auth:web,api')
 Route.delete('/tenders/:tender', 'TendersController.destroy').middleware('auth:web,api')
