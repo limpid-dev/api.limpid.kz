b51c4dc23c61dbe9aab3f97a27dc2d4b010e5a59
Azamat <augustintherome@gmail.com>
2023-04-23 22:48:01 +0600

    Made tenders controller

--- app/Controllers/Http/TendersController.ts
+++ app/Controllers/Http/TendersController.ts
@@ -1,3 +1,42 @@
+import { bind } from '@adonisjs/route-model-binding'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Profile from 'App/Models/Profile'
+import Tender from 'App/Models/Tender'
+import PaginationValidator from 'App/Validators/PaginationValidator'
+import ProfileActionValidator from 'App/Validators/ProfileActionValidator'
+import TenderStoreValidator from 'App/Validators/TenderStoreValidator'
 
-export default class TendersController {}
+export default class TendersController {
+  public async index({ request }: HttpContextContract) {
+    const payload = await request.validate(PaginationValidator)
+
+    return Tender.query()
+      .whereNotNull('verifiedAt')
+      .qs(request.qs())
+      .paginate(payload.page, payload.perPage)
+  }
+
+  @bind()
+  public async show({}: HttpContextContract, tender: Tender) {
+    return { data: tender }
+  }
+
+  public async store({ request, bouncer }: HttpContextContract) {
+    const payload = await request.validate(TenderStoreValidator)
+    const { profileId } = await request.validate(ProfileActionValidator)
+
+    const profile = await Profile.findOrFail(profileId)
+
+    await bouncer.with('TenderPolicy').authorize('create', profile)
+
+    const tender = await profile.related('tenders').create(payload)
+
+    return { data: tender }
+  }
+
+  @bind()
+  public async destroy({ bouncer }: HttpContextContract, tender: Tender) {
+    await bouncer.with('TenderPolicy').authorize('delete', tender)
+    await tender.delete()
+  }
+}
