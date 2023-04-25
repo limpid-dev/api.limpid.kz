54861cf46630a83d6add0996cdd8b8cf36aa208c
Azamat <augustintherome@gmail.com>
2023-04-25 10:54:18 +0600

    Fixed relationship

---
+++ database/migrations/1682397080307_users_files.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onDelete('SET NULL')
+        .nullable()
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropForeign('file_id')
+    })
+  }
+}
