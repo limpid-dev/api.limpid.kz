fb7da39de1ca77600aa3daee754d57e2ff9164b5
Alisher Utemurat <ali0701ute@gmail.com>
2023-05-15 00:17:23 +0600

    FIx

--- start/routes/payment.ts
+++ start/routes/payment.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('payment', 'PaymentController.paymentToken')
+Route.post('payment', 'PaymentController.paymentToken').middleware('auth:web,api')
 Route.post('accepted-payment', 'PaymentController.checkPaymentStatusAccepted')
 Route.post('refused-payment', 'PaymentController.checkPaymentStatusFailed')
