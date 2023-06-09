d7d14c345c7c61d6536363e62eae5b63a52b15a6
Azamat Zulpykhar <spender.poised-0q@icloud.com>
2023-06-10 03:33:11 +0600

    WIP

--- app/Models/Project.ts
+++ app/Models/Project.ts
@@ -12,7 +12,7 @@ import {
 import Profile from './Profile'
 import ProjectMember from './ProjectMember'
 import Chat from './Chat'
-import { AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
+import { AttachmentContract, attachment } from '@ioc:Adonis/Addons/AttachmentLite'
 
 export default class Project extends BaseModel {
   @column({ isPrimary: true })
@@ -60,16 +60,24 @@ export default class Project extends BaseModel {
   @column()
   public profitability: string
 
-  @column()
+  @attachment({
+    preComputeUrl: true,
+  })
   public logo: AttachmentContract | null
 
-  @column()
+  @attachment({
+    preComputeUrl: true,
+  })
   public videoIntroduction: AttachmentContract | null
 
-  @column()
+  @attachment({
+    preComputeUrl: true,
+  })
   public presentation: AttachmentContract | null
 
-  @column()
+  @attachment({
+    preComputeUrl: true,
+  })
   public businessPlan: AttachmentContract | null
 
   @column.dateTime({ autoCreate: true })
