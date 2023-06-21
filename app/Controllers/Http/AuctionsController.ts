e6ec37f703bbdc27da2733cfca8f41f2b5215eb7
Alisher Utemurat <ali0701ute@gmail.com>
2023-06-21 16:29:48 +0600

    Fix

--- app/Controllers/Http/AuctionsController.ts
+++ app/Controllers/Http/AuctionsController.ts
@@ -100,7 +100,7 @@ export default class AuctionsController {
 
   @bind()
   public async show({}: HttpContextContract, auction: Auction) {
-    const auctions = await Auction.query().preload('wonAuctionBid').preload('profile').where('id', auction.id)
+    const auctions = await Auction.query().preload('wonAuctionBid').preload('profile').where('id', auction.id).firstOrFail()
     console.log(auctions)
     return {
       data: auctions,
